42fec9346b32a73e51bc5c0d3a03261dbf3f3c20
mattz912 <mattz912345@gmail.com>
2023-11-01 16:03:21 -0500

    made mobile sizing changes and removed unnecessary extra code

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
-// import { data } from "./utils/data.js";
-// const projectContainer = document.querySelector(".project-container");
-// const project = document.createElement("div");
-// const projectItems = data.map((item) => {
-//   return (project.innerHTML = `<div><a data-bs-toggle="modal"
-//       data-bs-target="#exampleModal"
-//     ><img src="${item.image}" alt="${item.projectName}" class="project-modal" /></a>
+import { data } from "./utils/data.js";
+const projectContainer = document.querySelector(".project-area");
+const projects = document.createElement("div");
+projects.classList.add("project-container");
+const projectItems = data.map((item) => {
+  return `<a data-bs-toggle="modal" data-bs-target="#${item.id}"
+          ><img
+            src="${item.image}"
+            alt=""
+            class="project-image"
+        /><h4 class="text-center">${item.projectName}</h4></a>
+        
 
-//     <div
-//       class="modal fade"
-//       id="exampleModal"
-//       tabindex="-1"
-//       aria-labelledby="exampleModalLabel"
-//       aria-hidden="true"
-//     >
-//       <div class="modal-dialog">
-//         <div class="modal-content">
-//           <div class="modal-header">
-//             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-//             <button
-//               type="button"
-//               class="btn-close"
-//               data-bs-dismiss="modal"
-//               aria-label="Close"
-//             ></button>
-//           </div>
-//           <div class="modal-body">${item.projectName}</div>
-//           <div class="modal-footer">
-//             <button
-//               type="button"
-//               class="btn btn-secondary"
-//               data-bs-dismiss="modal"
-//             >
-//               Close
-//             </button>
-//             <button type="button" class="btn btn-primary">Save changes</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div></div>`);
-// });
-// // console.log(projectItems.length);
-// // console.log(project);
+        <!-- Modal -->
+        <div
+          class="modal fade"
+          id="${item.id}"
+          tabindex="-1"
+          aria-labelledby="${item.id}Label"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-color text-center">
+                <h1 class="modal-title" id="${item.id}Label">${item.projectName}</h1>
+                <button
+                  type="button"
+                  class="btn-close close-x"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body modal-color">
+                <p>
+                  ${item.description}
+                </p>
+                <ul class="modal-tools-container">
+                
+                  ${item.languages}
+                </ul>
+                <h3>Includes</h3>
+                <ul>
+                  ${item.included}
+                </ul>
+              </div>
+              <div class="modal-foot d-flex justify-content-evenly pb-5">
+                <div>
+                <a href="${item.githubLink}" target="_blank"><button type="button" class="link-button">GitHub</button> </a></div>
+                <div>
+                <a href="${item.websiteLink}" target="_blank"><button type="button" class="link-button">Website</button></a></div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+});
 
-// // for (let project of projectItems) {
-// //   let div = document.createElement("div");
-// //   div.innerHTML = project;
-// //   projectContainer.appendChild(projectItems);
-// // }
+for (let item of projectItems) {
+  projects.innerHTML += item;
+}
+
+projectContainer.appendChild(projects);
+// console.log(projectItems.length);
+// console.log(project);
+
+// for (let project of projectItems) {
+//   let div = document.createElement("div");
+//   div.innerHTML = project;
+//   projectContainer.appendChild(projectItems);
+// }
--- utils/data.js
+++ utils/data.js
@@ -1,75 +1,76 @@
 export const data = [
   {
     projectName: "DataMed",
+    id: "dataMedModal",
     image: "./images/VueProjectDataMed.jpg",
-    languages: [
-      {
-        language: "Vue",
-        icon: '<i class="fa-brands fa-vuejs" style="color: #42b883"></i>',
-      },
-      {
-        language: "Bootstrap",
-        icon: '<i class="fa-brands fa-bootstrap"></i>',
-      },
-      {
-        language: "Firebase",
-        icon: '<i class="fa-solid fa-database"></i>',
-      },
-    ],
-
+    languages: `<li>
+                    Vue
+                    <div><i class="fa-brands fa-vuejs"></i></div>
+                  </li>
+                  <li>
+                    Bootstrap
+                    <div><i class="fa-brands fa-bootstrap"></i></div>
+                  </li>
+                  <li>
+                    Firebase
+                    <div><i class="fa-solid fa-database firebase-color"></i></div>
+                  </li>`,
     description:
       "This project was created from a User Interface Design assignment. It uses a mix of Vue 2's Options API and Vue 3's Composition API",
-    included: [
-      "Firebase Login for multiple users with the ability for users to change their individual profile",
-      "API fetch to retrieve news data",
-      "Shopping cart that uses local storage",
-      "Vue Router",
-    ],
+    included: `<li>
+                    Firebase Login for multiple users with the ability for users
+                    to change their individual profile
+                  </li>
+                  <li>API fetch to retrieve news data</li>
+                  <li>Shopping cart that uses local storage</li>
+                  <li>Vue Router</li>`,
 
-    link: "",
+    githubLink: "https://github.com/mattcodings/data-med",
+    websiteLink: "https://mattcodings.github.io/data-med/#/",
   },
   {
     projectName: "Author Website",
+    id: "authorWebsiteModal",
     image: "./images/WordPressDevelopment.jpg",
-    languages: [
-      {
-        language: "WordPress",
-        icon: '<i class="fa-brands fa-vuejs" style="color: #42b883"></i>',
-      },
-      {
-        language: "PHP",
-        icon: '<i class="fa-brands fa-bootstrap"></i>',
-      },
-      {
-        language: "phpMyAdmin",
-        icon: '<i class="fa-solid fa-database"></i>',
-      },
-    ],
-
+    languages: `<li>
+                    WordPress
+                    <div><i class="fa-brands fa-wordpress"></i></div>
+                  </li>
+                  <li>
+                    PHP
+                    <div><i class="fa-brands fa-php"></i></div>
+                  </li>
+                  <li>
+                    MyAdmin
+                    <div><i class="fa-solid fa-database phpmyadmin-color"></i></div>
+                  </li>`,
     description:
       "This was the final project in WordPress Development. The theme was built from scratch using Underscores ",
-    included: [
-      "Custom Plugin with Shortcodes",
-      "OOP (Composition)",
-      "Sorting and Pagination ",
-    ],
-
-    link: "",
+    included: `<li>Custom Plugin with Shortcodes</li>
+                  <li>OOP (Composition)</li>
+                  <li>Sorting and Pagination</li>`,
+    githubLink: "https://github.com/mattcodings/author-website",
+    websiteLink: "https://mzwerlein.bitlampsites.com/wpd/final/wordpress/",
   },
   {
     projectName: "BrightHouse",
+    id: "brightHouseModal",
     image: "./images/ReactProjectBrightHouse.jpg",
-    languages: [
-      {
-        language: "React",
-        icon: '<i class="fa-brands fa-react"></i>',
-      },
-    ],
+    languages: `<li>
+                    React
+                    <div><i class="fa-brands fa-react"></i></div>
+                  </li>
+                  <li>
+                    Vanilla JS
+                    <div><i class="fa-brands fa-js"></i></div>
+                  </li>`,
 
     description:
       "This was a project in User Interface Design. Please note: I have yet to take the React class at WCTC. This project was the result from self-teaching, and was created to learn state hooks and the React router. ",
-    included: ["Hooks", "React Router"],
+    included: `<li>Hooks</li>
+                  <li>React Router</li>`,
 
-    link: "",
+    githubLink: "https://github.com/mattcodings/brighthouse",
+    websiteLink: "https://mattcodings.github.io/brighthouse/",
   },
 ];
