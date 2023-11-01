9aebf02567bbf865c458a4c3bb2a780f99be8022
mattz912 <mattz912345@gmail.com>
2023-11-01 00:24:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// import { data } from "./utils/data.js";
+// const projectContainer = document.querySelector(".project-container");
+// const project = document.createElement("div");
+// const projectItems = data.map((item) => {
+//   return (project.innerHTML = `<div><a data-bs-toggle="modal"
+//       data-bs-target="#exampleModal"
+//     ><img src="${item.image}" alt="${item.projectName}" class="project-modal" /></a>
+
+//     <div
+//       class="modal fade"
+//       id="exampleModal"
+//       tabindex="-1"
+//       aria-labelledby="exampleModalLabel"
+//       aria-hidden="true"
+//     >
+//       <div class="modal-dialog">
+//         <div class="modal-content">
+//           <div class="modal-header">
+//             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+//             <button
+//               type="button"
+//               class="btn-close"
+//               data-bs-dismiss="modal"
+//               aria-label="Close"
+//             ></button>
+//           </div>
+//           <div class="modal-body">${item.projectName}</div>
+//           <div class="modal-footer">
+//             <button
+//               type="button"
+//               class="btn btn-secondary"
+//               data-bs-dismiss="modal"
+//             >
+//               Close
+//             </button>
+//             <button type="button" class="btn btn-primary">Save changes</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div></div>`);
+// });
+// // console.log(projectItems.length);
+// // console.log(project);
+
+// // for (let project of projectItems) {
+// //   let div = document.createElement("div");
+// //   div.innerHTML = project;
+// //   projectContainer.appendChild(projectItems);
+// // }
---
+++ utils/data.js
@@ -0,0 +1,75 @@
+export const data = [
+  {
+    projectName: "DataMed",
+    image: "./images/VueProjectDataMed.jpg",
+    languages: [
+      {
+        language: "Vue",
+        icon: '<i class="fa-brands fa-vuejs" style="color: #42b883"></i>',
+      },
+      {
+        language: "Bootstrap",
+        icon: '<i class="fa-brands fa-bootstrap"></i>',
+      },
+      {
+        language: "Firebase",
+        icon: '<i class="fa-solid fa-database"></i>',
+      },
+    ],
+
+    description:
+      "This project was created from a User Interface Design assignment. It uses a mix of Vue 2's Options API and Vue 3's Composition API",
+    included: [
+      "Firebase Login for multiple users with the ability for users to change their individual profile",
+      "API fetch to retrieve news data",
+      "Shopping cart that uses local storage",
+      "Vue Router",
+    ],
+
+    link: "",
+  },
+  {
+    projectName: "Author Website",
+    image: "./images/WordPressDevelopment.jpg",
+    languages: [
+      {
+        language: "WordPress",
+        icon: '<i class="fa-brands fa-vuejs" style="color: #42b883"></i>',
+      },
+      {
+        language: "PHP",
+        icon: '<i class="fa-brands fa-bootstrap"></i>',
+      },
+      {
+        language: "phpMyAdmin",
+        icon: '<i class="fa-solid fa-database"></i>',
+      },
+    ],
+
+    description:
+      "This was the final project in WordPress Development. The theme was built from scratch using Underscores ",
+    included: [
+      "Custom Plugin with Shortcodes",
+      "OOP (Composition)",
+      "Sorting and Pagination ",
+    ],
+
+    link: "",
+  },
+  {
+    projectName: "BrightHouse",
+    image: "./images/ReactProjectBrightHouse.jpg",
+    languages: [
+      {
+        language: "React",
+        icon: '<i class="fa-brands fa-react"></i>',
+      },
+    ],
+
+    description:
+      "This was a project in User Interface Design. Please note: I have yet to take the React class at WCTC. This project was the result from self-teaching, and was created to learn state hooks and the React router. ",
+    included: ["Hooks", "React Router"],
+
+    link: "",
+  },
+];
